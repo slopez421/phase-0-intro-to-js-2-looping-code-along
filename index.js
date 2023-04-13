bb874654d8a04af1452b25c677628acc3b72529e
Shania Lopez <slopez@fandm.edu>
2023-04-13 19:07:14 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ["Shania", "Jeffrey", "Joshua"]
+
+function writeCards(names, event) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+    } 
+
+    return messages
+};
+
+function countDown() {
+    let i = 10; while (i >= 0) {
+            console.log(i--);
+        }
+}
